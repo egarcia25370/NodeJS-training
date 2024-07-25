f9990f448ab68c68103fc9c322ee8aae0d9dac80
egarcia25370 <egarcia25370@gmail.com>
2024-07-25 16:01:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("Training NodeJS !!!")
+class myAPI
+{
+    processGet(req, res)
+    {
+        count++
+        res.send(`Requête : ${count}`)
+    }
+
+    processLaunch()
+    {
+        console.log(`Application lancée sur : http://localhost:${port}`)
+    }
+}
+
+const express = require("express")
+
+const h = express()
+const port = 3000
+
+count = 0
+
+a = new myAPI()
+
+h.get('/', a.processGet)
+
+h.listen(port, a.processLaunch)
+
+return 0
